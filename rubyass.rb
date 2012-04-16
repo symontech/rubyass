1746e1ea1b01baabe73d7145d252a6737b89dda7
Simon Wepfer <sw@netsense.ch>
2012-04-16 17:24:41 +0200

    prototype

--- rubyass.rb
+++ rubyass.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+require 'optparse'
 require 'yaml'
 require 'term/ansicolor'
 include Term::ANSIColor
@@ -7,8 +9,6 @@ def hr
   80.times { print '-' }
 	puts reset
 end
-
-hr
 
 def error(message)
   puts red, bold, "ERROR: #{message}", reset
@@ -24,25 +24,63 @@ def replace_vars(cmd, config)
 	return cmd
 end
 
+options = {
+  :first       => "default value",
+  :another     => 23,
+  :bool        => false,
+  :list        => ["x", "y", "z"],
+  :dest        => File.expand_path(File.dirname($0))
+}
+
+ARGV.options do |o|
+  script_name = File.basename($0)
+  
+  o.set_summary_indent('  ')
+  o.banner =    "Usage: #{script_name} [OPTIONS] target"
+  o.define_head "ruby basic security assessment"
+  o.separator   ""
+  #o.separator   "Mandatory arguments to long options are mandatory for " +
+  #              "short options too."  
+#  o.on("-f", "--first=[val]", String,
+#       "A long and short (optional) string argument",
+#       "Default: #{options[:first]}")   { |options[:first]| }
+#  o.on("-a", "--another=val", Integer,
+#       "Requires an int argument")      { |options[:another]| }
+#  o.on("-b", "--boolean",
+#       "A boolean argument")            { |options[:bool]| }
+#  o.on("--list=[x,y,z]", Array, 
+#       "Example 'list' of arguments")   { |options[:list]| }
+  
+  #o.separator ""
+
+  o.on_tail("-h", "--help", "Show this help message.") { puts o; puts; exit }
+  o.parse!
+end
+
+# target is required
+unless ARGV.last
+  error "no target"
+  exit 1 
+end
+
 # load the tests from yaml
 tests = YAML::load_file('tests.yml') rescue nil
 if tests
   success "#{tests.length} tests loaded"
 else
-  error "no tests could be loaded"
-	exit 1
+  error "no tests could be loaded" and exit 1
 end
 
 config = {
-	:target => '',
+	:target => ARGV.last,
 	:open_ports => {:tcp => [], :udp => [] },
 }
 
 # test config
-config[:target] = "127.0.0.1"
+#config[:target] = "127.0.0.1"
 config[:open_ports][:tcp] << 22 
 
-puts bold, "target ip: #{config[:target]}", reset
+puts bold, "Target: #{config[:target]}", reset
 
 # start the tests
 tests.each do |test|
