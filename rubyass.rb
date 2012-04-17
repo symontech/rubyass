6d7a698311f56519464cb2d3b7a9c4345a97364d
Simon Wepfer <sw@netsense.ch>
2012-04-17 10:35:34 +0200

    added checks

--- rubyass.rb
+++ rubyass.rb
@@ -5,9 +5,9 @@ require 'term/ansicolor'
 include Term::ANSIColor
 
 def hr
-	print blue
+  print blue
   80.times { print '-' }
-	puts reset
+  puts reset
 end
 
 def error(message)
@@ -19,9 +19,10 @@ def success(message)
 end
 
 def replace_vars(cmd, config)
-  cmd.gsub!(/\$target/, config[:target])
-  cmd.gsub!(/\$port_open/, config[:open_ports][:tcp].first.to_s)
-	return cmd
+  cmd.gsub!(/\$target/, 	config[:target])
+  cmd.gsub!(/\$port_open/, 	config[:open_ports][:tcp].first.to_s)
+  cmd.gsub!(/\$repeat/, 	config[:repeat].to_s)
+  return cmd
 end
 
 options = {
@@ -73,34 +74,62 @@ end
 
 config = {
 	:target => ARGV.last,
+	:repeat => 3,
 	:open_ports => {:tcp => [], :udp => [] },
 }
 
-# test config
-#config[:target] = "127.0.0.1"
-config[:open_ports][:tcp] << 22 
 
+# test config
+config[:open_ports][:tcp] << 80 
+
+# display target
 puts bold, "Target: #{config[:target]}", reset
 
 # start the tests
 tests.each do |test|
+  # replace vars and show command
+  print yellow, "#{test[:name]}"
+  cmd = replace_vars(test[:command], config)
+  puts green, "  #{cmd}", reset
 
-	# replace vars and show command
-  puts yellow, "#{test[:name]}", reset
-	cmd = replace_vars(test[:command], config)
-	puts green, cmd, reset
-
-	# run command and capture output
+  # run command and capture output
   output = "" 
-	IO.popen (cmd) do |f| 
-		while(s = f.gets)
-	    puts s 
-			output += s
-		end
-	end
+  IO.popen (cmd) do |f|
+    while(s = f.gets)
+      output += s
+    end
+  end
 
+  #puts output
 
-	puts; hr;
+  # run checks on output
+  if test[:checks]
+    test[:checks].each do |check|
+      # prepare output
+      puts; puts
+      print bold, check[:name], reset
+      forward = 80 - check[:name].length
+      forward = 1 if forward < 1
+      forward.times { print " " }
+      print "["
+
+      # eval
+      matches = output.match(Regexp.new(check[:test]))
+      passed = true if matches
+      passed = !passed unless check[:match_passes]
+      if passed
+        print green, "OK"
+      else
+        print red, "FAILED"
+      end
+      print reset, "]\n"
+      print matches
+
+    end
+  end
+
+  # seperator
+  puts; hr;
 end
 
 exit 0
